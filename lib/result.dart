5d0e266f703a48f3299ac0141cce93722ab4a9f5
Deepak Kumar <deepakstark23@gmail.com>
2021-12-15 09:51:50 +0530

    first commit

---
+++ lib/result.dart
@@ -0,0 +1,64 @@
+import 'package:bmi_calculator/main.dart';
+import 'package:flutter/material.dart';
+import 'constant.dart';
+import 'Resuable_card.dart';
+import 'bottom_button.dart';
+
+class ResultPage extends StatelessWidget {
+
+ResultPage({@required this.bmiResult,@required this.interpretation,@required this.resultText});
+
+final String bmiResult;
+final String resultText;
+final String interpretation;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body:Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Expanded(
+            child: Text(
+              'Your Result',
+              style: kTitleTextStyle,
+            ),
+          ),
+          Expanded(
+            flex:5,
+            child: ReusableCard(
+              colour:kActiveCardColor,
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    resultText.toUpperCase(),
+                    style: kResultTextStyle,
+                  ),
+                  Text(
+                    bmiResult,
+                    style: kBMITextStyle,
+                  ),
+                  Text(
+                    interpretation,
+                    textAlign: TextAlign.center,
+                    style: kBodyTextStyle,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          BottomButton(
+            buttonTitle: 'RE-CALCULATE', 
+            onTap:(){
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
